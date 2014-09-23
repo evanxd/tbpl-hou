e50e843f0fa2faa7f2d7b591f3273cffef538820
Evan Xd <itoyxd@gmail.com>
2014-09-23 14:53:30 +0800

    Add tracking sign as a border of the container element

--- js/check_result.js
+++ js/check_result.js
@@ -2,22 +2,41 @@
 
 /* global TbplJob, Notification */
 
-setTimeout(checkResult, 5000);
+(function() {
+  var container = document.querySelector('#container');
 
-function checkResult() {
-  var tbpl = new TbplJob();
-  var result = 'Fail!';
-  if (tbpl.isDone()) {
-    if (tbpl.isSuccessful()) {
-      result = 'Success!';
+  addTrackingSign();
+  setTimeout(checkResult, 5000);
+
+  function checkResult() {
+    var tbpl = new TbplJob();
+    var result = 'Fail!';
+    if (tbpl.isDone()) {
+      if (tbpl.isSuccessful()) {
+        result = 'Success!';
+      }
+
+      Notification.requestPermission(function() {
+        var notification = new Notification(tbpl.description, { body: result });
+        notification.onclick = function() {
+          window.focus();
+        };
+      });
+
+      removeTrackingSign();
+    } else {
+      setTimeout(checkResult, 5000);
     }
-    Notification.requestPermission(function() {
-      var notification = new Notification(tbpl.description, { body: result });
-      notification.onclick = function() {
-        window.focus();
-      };
-    });
-  } else {
-    setTimeout(checkResult, 5000);
   }
-}
+
+  function addTrackingSign() {
+    container.style.borderStyle = 'dashed';
+    // Firefox light orange, https://www.mozilla.org/en-US/styleguide/identity/firefox/color/.
+    container.style.borderColor = '#FF9500';
+  }
+
+  function removeTrackingSign() {
+    container.style.borderStyle = null;
+    container.style.borderColor = null;
+  }
+})();
