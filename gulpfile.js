6321c4d7e2204abc93b58c8f296791bf52788a3c
Evan Xd <itoyxd@gmail.com>
2014-09-24 23:48:46 +0800

    Refactor Firefox Add-ons build script

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,35 @@
 var gulp = require('gulp'),
     clean = require('gulp-clean'),
     download = require('gulp-download'),
-    exec = require('sync-exec'),
-    unzip = require('gulp-unzip');
+    fs = require('fs'),
+    unzip = require('gulp-unzip'),
+    sh = require('execSync'),
+    Q = require('q');
 
 gulp.task('build', function() {
-  download('https://ftp.mozilla.org/pub/mozilla.org/labs/jetpack/jetpack-sdk-latest.zip')
-    .pipe(gulp.dest('build'))
-    .pipe(unzip())
-    .pipe(gulp.dest('build'))
-    .on('end', function() {
-      gulp.src('js/**/*.js')
-        .pipe(gulp.dest('firefox-addon/data'))
-        .on('end', function() {
-          exec('./build/addon-sdk*/bin/cfx xpi --pkgdir=./firefox-addon/');
-          gulp.src('tbpl-hou.xpi')
-            .pipe(clean())
-            .pipe(gulp.dest('build'));
-        });
-    });
+  (function() {
+    // Download Firefox Add-ons SDK.
+    var deferred = Q.defer();
+    if (fs.existsSync(__dirname + '/build/addon-sdk-1.17/bin/cfx')) {
+      deferred.resolve();
+    } else {
+      download('https://ftp.mozilla.org/pub/mozilla.org/labs/jetpack/jetpack-sdk-latest.zip')
+        .pipe(unzip())
+        .pipe(gulp.dest('build'))
+        .on('end', deferred.resolve);
+    }
+    return deferred.promise;
+  })()
+  .then(function() {
+    // Move tbpl-hou script into firefox-addon folder.
+    gulp.src('js/**/*.js')
+      .pipe(gulp.dest('firefox-addon/data'));
+  })
+  .then(function() {
+    // Build it.
+    sh.run('./build/addon-sdk-1.17/bin/cfx xpi --pkgdir=./firefox-addon/');
+    gulp.src('tbpl-hou.xpi')
+      .pipe(clean())
+      .pipe(gulp.dest('build'));
+  });
 });
