6ab8c398424cc094becef74bff328cf7249f2e53
Evan Xd <itoyxd@gmail.com>
2014-09-26 09:42:48 +0800

    Issue 1 - Match different URLs to inject script in Add-ons and Extension

--- firefox-addon/lib/main.js
+++ firefox-addon/lib/main.js
@@ -5,8 +5,11 @@ var ContentScript = require('./content_script').ContentScript;
 
 tabs.on('ready', function(tab) {
   var contentScript;
-  // Inject the script if we are in the TBPL job page.
-  if (tab.url.indexOf('https://tbpl.mozilla.org/?rev=') !== -1) {
+  // If url matches https://tbpl.mozilla.org/?rev=*&tree=Gaia-Try or
+  // https://tbpl.mozilla.org/?tree=Gaia-Try&rev=*,
+  // inject the script if we are in the TBPL job page.
+  if (tab.url.match(/^https:\/\/tbpl.mozilla.org\/\?rev=\w+\x26tree=Gaia-Try/) ||
+      tab.url.match(/^https:\/\/tbpl.mozilla.org\/\?tree=Gaia-Try\x26rev=\w+/)) {
     contentScript = new ContentScript(tab);
     contentScript.inject(['tbpl_job.js', 'check_result.js']);
   }
